06f004c4a1a438b05a1fe0f37535acc6abd28a9a
unknown <user@a5comp3.step.itstep.rv.ua>
2015-05-05 21:52:14 +0300

    no message

---
+++ Lesson2/HomeWork1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < 10; i++)
+			{
+			 list.Add(i+" thread");
+			}
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(Method);
+            Thread t1 = new Thread(ts);
+            t1.Start(list);
+            
+        }
+        static void Method(Object a)
+        {
+        foreach (var el in (a as List<string>))
+        {
+            Console.WriteLine(el);
+
+        }
+    
+   
+    
+    }
+    }
+}
---
+++ Lesson2/Join/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Join
+{
+    class Program
+    {
+        static double mainsum, threadSum, sum;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Medhod);
+            t1.IsBackground=true;
+            t1.Start();
+            //t1.Join();
+            
+            for (int i = 0; i < 100; i++)
+            {
+                mainsum += (i + 1);
+            }
+
+            sum = mainsum + threadSum;
+            Console.WriteLine("sum " + sum);
+
+
+        }
+
+        static void Medhod()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                threadSum += (i + 1);
+                Thread.Sleep(1);
+            }
+        }
+    }
+
+}
