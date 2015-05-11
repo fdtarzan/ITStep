782cdef95a2ab75c86a7fc60e58a6536f5d95f4b
fdtarzan <fdtarzan@gmail.com>
2015-05-11 16:30:31 +0300

    ProcessTree add

---
+++ Lesson1HomeWork4/ProcessTree/ProcessTree/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ProcessTree
+{
+    partial class MForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(1, -5);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(294, 279);
+            this.treeView1.TabIndex = 0;
+            // 
+            // MForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 273);
+            this.Controls.Add(this.treeView1);
+            this.Name = "MForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+
+    }
+}
+
---
+++ Lesson1HomeWork4/ProcessTree/ProcessTree/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using System.Collections.Generic;
+
+
+namespace ProcessTree
+{
+    public partial class MForm : Form
+    {
+        List<int> parent;
+        List<Process> parentProc;
+       
+        public MForm()
+        {
+            
+            InitializeComponent();
+            Process[] p = Process.GetProcesses();
+            parent = new List<int>();
+            parentProc = new List<Process>();
+            
+
+            foreach (Process el in p)
+            {
+                if ( GetParentId(el)!=null && !parent.Exists(c => c == GetParentId(el)))
+                {
+                    try
+                    {
+                        parent.Add((int)GetParentId(el));
+                        parentProc.Add(Process.GetProcessById((int)GetParentId(el)));
+                        
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                    
+                    }
+                }
+            }
+          //   MessageBox.Show(parent.Count.ToString());
+
+             foreach (var el in parentProc)
+            {
+               
+                 TreeNode tn = new TreeNode(el.ProcessName);
+
+                 treeView1.Nodes.Add(tn);
+                foreach (var e in GetChildProcesses(el))
+                {
+                    tn.Nodes.Add(e.ProcessName);
+                }
+               
+            }
+          //  MessageBox.Show(parentProc.Count.ToString());
+           
+
+        }
+         public int? GetParentId(Process process)
+        {
+        // query the management system objects
+        string queryText = string.Format("select parentprocessid from win32_process where processid = {0}", process.Id);
+        using (var searcher = new ManagementObjectSearcher(queryText))
+        {
+            foreach (var obj in searcher.Get())
+            {
+                object data = obj.Properties["parentprocessid"].Value;
+                if (data != null)
+                    return Convert.ToInt32(data);
+            }
+        }
+        return null;
+        }
+         public  List<Process> GetChildProcesses(Process process)
+         {
+             var results = new List<Process>();
+
+             // query the management system objects for any process that has the current
+             // process listed as it's parentprocessid
+             string queryText = string.Format("select processid from win32_process where parentprocessid = {0}", process.Id);
+             using (var searcher = new ManagementObjectSearcher(queryText))
+             {
+                 foreach (var obj in searcher.Get())
+                 {
+                     object data = obj.Properties["processid"].Value;
+                     if (data != null)
+                     {
+                         // retrieve the process
+                         var childId = Convert.ToInt32(data);
+                         var childProcess = Process.GetProcessById(childId);
+
+                         // ensure the current process is still live
+                         if (childProcess != null)
+                             results.Add(childProcess);
+                     }
+                 }
+             }
+             return results;
+         }
+
+            
+    }
+      
+
+}
