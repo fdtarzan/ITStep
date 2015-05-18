5ef18a6009b28693541118b56a3801aaa18ba1db
unknown <user@a5comp3.step.itstep.rv.ua>
2015-05-18 21:51:08 +0300

    sss

---
+++ Lesson5/GCTimer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GCTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object ob = new object();
+            Timer t = new Timer(new TimerCallback(Print),ob,1000,1000);
+            t.Change(500, 500);
+            Console.ReadLine();
+        }
+        static void Print(Object s)
+        {
+           
+                Console.WriteLine("dddd");
+                GC.Collect();
+
+           
+        }
+    }
+}
