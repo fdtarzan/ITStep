4f35b105cafc4e5e60e8de571d3ce12bc4e6488a
unknown <user@a5comp3.step.itstep.rv.ua>
2015-05-27 21:45:04 +0300

    VK Api

---
+++ Lesson1HomeWork5/Lesson8/Json/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Json
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 443);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(735, 31);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 303);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(261, 119);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(234, 303);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(74, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Online";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(334, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Offline";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 474);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Lesson1HomeWork5/Lesson8/Json/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Json
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          //  webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=4935014&scope=friends&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&v=5.33&state=SESSION_STATE&display=popup");
+
+
+            WebRequest request = WebRequest.Create("https://api.vk.com/method/friends.get?user_id=8436362&v=5.33&order=name&fields=city,domain&name_case=nom&access_token=899b4be230864233a0539e64d2d491cf39f4bf539734b8b3d30d40c125c4b48b7f88fc6f3269d3b770419");
+            WebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+           
+            StreamReader reader = new StreamReader(dataStream);
+           
+            string responseFromServer = reader.ReadToEnd();
+            //richTextBox1.Text = responseFromServer;
+
+            FriendsInfo fid = JsonConvert.DeserializeObject<FriendsInfo>(responseFromServer);
+            foreach (var el in fid.response.items)
+            {
+                if (el.online == 1)
+                {
+                    listBox1.Items.Add(el.first_name +" "+ el.last_name);
+                }
+                else
+                {
+                    listBox2.Items.Add(el.first_name + " " + el.last_name);
+                }
+                
+            
+            }
+
+           
+            //MessageBox.Show(responseFromServer);
+
+
+
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.AbsoluteUri;
+
+
+          //  899b4be230864233a0539e64d2d491cf39f4bf539734b8b3d30d40c125c4b48b7f88fc6f3269d3b770419
+        }
+      
+    }
+
+
+
+    public class City
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Item
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string domain { get; set; }
+        public City city { get; set; }
+        public int online { get; set; }
+        public string deactivated { get; set; }
+        public List<int?> lists { get; set; }
+    }
+
+    public class Response
+    {
+        public int count { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+    public class FriendsInfo
+    {
+        public Response response { get; set; }
+    }
+}
