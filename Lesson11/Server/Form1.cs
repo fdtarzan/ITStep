e96e8203ebe2d22198232693ffa60b89ec9cbbbd
unknown <user@a5comp3.step.itstep.rv.ua>
2015-06-08 21:36:25 +0300

    l11

---
+++ Lesson11/Server/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Command("Lock");
+        }
+
+
+        private void Command(string com)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(IPAddress.Broadcast, 10000);
+            byte[] bytes = Encoding.UTF8.GetBytes(com);
+            udpClient.Send(bytes, bytes.Length);
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Command("Shutdown");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Command("CancelShutdown");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Command("Ololo");
+        }
+    }
+}
---
+++ Lesson11/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+            Thread t = new Thread(Listen);
+            t.IsBackground = true;
+            t.Start();
+
+        }
+
+        public void Listen()
+        {
+            while (true)
+            {
+                using (UdpClient cl = new UdpClient(10000))
+                {
+                    IPEndPoint RemoteIPEndPoint = null;
+                    byte[] bytes = cl.Receive(ref RemoteIPEndPoint);
+                    string results = Encoding.UTF8.GetString(bytes);
+                    if (results == "Lock")
+                    {
+                        Process.Start("rundll32.exe", "user32.dll, LockWorkStation");
+                    }
+                    if (results == "Shutdown")
+                    {
+                        Process.Start("shutdown", "-s -t 60 -c \"Go to HELL!!!!!\"");
+                    }
+                    if (results == "CancelShutdown")
+                    {
+                        Process.Start("shutdown", "-a");
+                    }
+                    if (results == "Ololo")
+                    {
+                      //  for (int i = 0; i < 20; i++)
+                      //  {
+                      //      Process.Start("notepad");
+                      //      Process.Start("calc");
+                      //  }
+                        IntPtr hDC = GetDC(IntPtr.Zero);
+                        using (Graphics g = Graphics.FromHdc(hDC))
+                        {
+                            var fontt=new Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                            g.DrawString("SAY HELLO VIRUS!!!", fontt, Brushes.Red, new Random().Next(0, 300), new Random().Next(0, 800));
+                        }
+                        ReleaseDC(IntPtr.Zero, hDC);
+                    }
+                }
+            }
+
+        
+        }
+    }
+}
