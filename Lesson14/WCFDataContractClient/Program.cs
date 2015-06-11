15b2f368b9da82b132e2de070e1c079fb2e7dec3
unknown <user@a5comp3.step.itstep.rv.ua>
2015-06-11 21:56:08 +0300

    l14

---
+++ Lesson14/WCFDataContractClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFDataContractClient.ServiceNS;
+//using WCFDataContractClient.ServiceAsyncNS;
+
+namespace WCFDataContractClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDiskInfoClient dc = new MyDiskInfoClient();
+            bool ok=true;
+            while (ok)
+            {
+                Console.Write("Enter disk: ");
+                string disk = Console.ReadLine();
+
+                if (disk.Length>0&&dc.DiskTotal(disk[0].ToString()) != null)
+                {
+                    var res = dc.DiskTotal1Async(disk[0].ToString());
+                    Console.WriteLine("Free Space:  {0}", res.Result.freeSpace);
+                  //  Console.WriteLine("Free Space:  {0}", dc.DiskTotal1(disk[0].ToString()).freeSpace);
+                  //  Console.WriteLine("Total space: {0}", dc.DiskTotal1(disk[0].ToString()).totalSpace);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+    }
+}
---
+++ Lesson14/WCFDataContractService/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace WCFDataContractService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(MyDiskInfo), new Uri("http://localhost/MyDiskInfo/"));
+            // sh.AddServiceEndpoint(typeof(IMyDiskInfo), new WSHttpBinding(), "Ep1");
+
+            //  ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            // behavior.HttpGetEnabled = true;
+            // sh.Description.Behaviors.Add(behavior);
+            // sh.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+            sh.Open();
+
+            Console.WriteLine("-------------------");
+            Console.ReadLine();
+            sh.Close();
+        }
+
+    }
+
+    [ServiceContract]
+    public interface IMyDiskInfo
+    {
+        [OperationContract (Name="DiskTotal1")]
+        DiskInfoResult DiskInfoTotal(string disk);
+        [OperationContract (Name = "DiskTotal2")]
+        DiskInfoResult DiskInfoTotal(string disk, string disk1);
+
+    }
+    [DataContract]
+    public class DiskInfoResult
+    {
+        [DataMember]
+        public string freeSpace;
+        [DataMember]
+        public string totalSpace;
+    
+    }
+
+
+    public class MyDiskInfo : IMyDiskInfo
+    {
+
+
+        public DiskInfoResult DiskInfoTotal(string disk)
+        {
+            try
+            {
+                DiskInfoResult dir = new DiskInfoResult();
+
+                DriveInfo d = new DriveInfo(disk);
+                dir.totalSpace=d.TotalSize.ToString();
+                dir.freeSpace = d.AvailableFreeSpace.ToString();
+                return dir;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public DiskInfoResult DiskInfoTotal(string disk, string disk1)
+        {
+            try
+            {
+                DiskInfoResult dir = new DiskInfoResult();
+
+                DriveInfo d = new DriveInfo(disk);
+                dir.totalSpace = d.TotalSize.ToString();
+                dir.freeSpace = d.AvailableFreeSpace.ToString();
+                return dir;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+       
+    }
+}
